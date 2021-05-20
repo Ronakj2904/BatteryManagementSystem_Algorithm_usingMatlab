95bd67ce9b8378bec162cf172c5c9c51863a06c4
Ronakj2904 <84306547+Ronakj2904@users.noreply.github.com>
2021-05-20 23:20:11 +0530

    Update tuneModel.m

--- ElectrochemicalCellModel/tuneModel.m
+++ ElectrochemicalCellModel/tuneModel.m
@@ -1,20 +1,19 @@
 %%
-% GRADED FUNCTION (do not modify this line)
 
 % function rcValues = tuneModel
 %
 % This function specifies the resistor and capacitor values that
-% you choose to use for either an Rint model or a Thévenin model 
-% (see lesson 2.1.3), or an "extended Thévenin model" having 
+% you choose to use for either an Rint model or a ThÃ©venin model 
+% (see lesson 2.1.3), or an "extended ThÃ©venin model" having 
 % additional parallel resistor-capacitor branches.
 % 
 % If you wish to create an Rint model, simply set rcValues equal
 % to the value of R0 (in milliohms).
-% If you wish to create a Thévenin model, define rcValues to be 
+% If you wish to create a ThÃ©venin model, define rcValues to be 
 % a vector having three elements. The first element is R0 (in
 % milliohms), the second element is R1 (in milliohms), and the 
 % third element is C1 (in kilofarads).
-% If you wish to create an extended Thévenin model having 
+% If you wish to create an extended ThÃ©venin model having 
 % additional resistor-capacitor branches, then define rcValues to
 % be a vector where the first element is R0 in milliohms, the 
 % even-index elements (rcValues(2:2:end)) are resistor values for
@@ -23,8 +22,8 @@
 % the resistor-capacitor branches, in kilofarads.
 %
 % It is possible to receive full credit for this assignment with
-% a well-tuned Thévenin model, but it is also possible to get a 
-% much better fit to the data using an extended Thévenin model.
+% a well-tuned ThÃ©venin model, but it is also possible to get a 
+% much better fit to the data using an extended ThÃ©venin model.
 function rcValues = tuneModel
 
   % BEGIN MODIFYING CODE AFTER THIS
